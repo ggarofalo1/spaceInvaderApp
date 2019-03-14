d90f9b05f2c38a0e94a541f10186c5d60c4b33b7
ggarofalo1 <ggarofalo1@users.noreply.github.com>
2019-03-13 23:15:28 -0400

    the starting class of the project

--- app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
+++ app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.spaceisgreat.www.invaderapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //sets game to fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //sets Title of App Off
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
