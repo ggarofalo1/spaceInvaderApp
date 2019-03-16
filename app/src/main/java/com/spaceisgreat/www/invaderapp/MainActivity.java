5f2c3e23c0f0662d30ce36cb0596dd68f93fce1b
ggarofalo1 <ggarofalo1@users.noreply.github.com>
2019-03-15 21:40:40 -0400

    added comment

--- app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
+++ app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
-    //test
+//test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
