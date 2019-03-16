f5d363d4cbfb2ba071fa59988afb5899c5bda619
ggarofalo1 <ggarofalo1@users.noreply.github.com>
2019-03-16 15:45:19 -0400

    added things to settings menu game setting are set, but others are not, need thread pause for game

--- app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
+++ app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
@@ -1,12 +1,15 @@
 package com.spaceisgreat.www.invaderapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 //test
@@ -18,11 +21,10 @@ public class MainActivity extends AppCompatActivity {
         //sets game to fullscreen
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //sets Title of App Off
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         this.setContentView(new GameSurface(this));
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,8 +33,27 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.newgame:
+                //Do something for help
+                Toast.makeText(getApplicationContext(), "New Game Started", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.settings:
+                //start settings activity
+                Intent settings = new Intent(this, SettingsActivity.class);
+                startActivity(settings);
+                break;
+            case R.id.help:
+                //Do something for help
+                Toast.makeText(getApplicationContext(), "Help", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                //error with selection
+        }
         return super.onOptionsItemSelected(item);
     }
 }
