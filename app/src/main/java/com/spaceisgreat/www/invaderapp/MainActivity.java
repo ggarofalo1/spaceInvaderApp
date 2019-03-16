eba5225b30ab9f9863dc0a029ab893df6332c1bd
ggarofalo1 <ggarofalo1@users.noreply.github.com>
2019-03-16 16:10:43 -0400

    added permissions and accelarometer code

--- app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
+++ app/src/main/java/com/spaceisgreat/www/invaderapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.spaceisgreat.www.invaderapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,7 +18,17 @@ import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-//test
+    private float acceleration;
+    private float currentAcceleration;
+    private float lastAcceleration;
+    private boolean dialogOnScreen = false;
+
+    // value used to determine whether user shook the device to erase
+    private static final int ACCELERATION_THRESHOLD = 100000;
+
+    // used to identify the request for using external storage, which
+    // the save image feature needs
+    private static final int SAVE_RECORD_PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         this.setContentView(new GameSurface(this));
+
+        // initialize acceleration values
+        acceleration = 0.00f;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
     }
 
 
@@ -55,5 +76,94 @@ public class MainActivity extends AppCompatActivity {
                 //error with selection
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // start listening for sensor events
+    @Override
+    public void onResume() {
+        super.onResume();
+        enableAccelerometerListening(); // listen for shake event
+    }
+
+    // enable listening for accelerometer events
+    private void enableAccelerometerListening() {
+        // get the SensorManager
+        SensorManager sensorManager =
+                (SensorManager) this.getSystemService(
+                        Context.SENSOR_SERVICE);
+
+        // register to listen for accelerometer events
+        sensorManager.registerListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    // stop listening for accelerometer events
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableAccelerometerListening(); // stop listening for shake
+    }
+
+    // disable listening for accelerometer events
+    private void disableAccelerometerListening() {
+        // get the SensorManager
+        SensorManager sensorManager =
+                (SensorManager) this.getSystemService(
+                        Context.SENSOR_SERVICE);
+
+        // stop listening for accelerometer events
+        sensorManager.unregisterListener(sensorEventListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+    }
+
+    // event handler for accelerometer events
+    private final SensorEventListener sensorEventListener =
+            new SensorEventListener() {
+                // use accelerometer to determine whether user shook device
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    // ensure that other dialogs are not displayed
+                    if (!dialogOnScreen) {
+                        // get x, y, and z values for the SensorEvent
+                        float x = event.values[0];
+                        float y = event.values[1];
+                        float z = event.values[2];
+
+                        // save previous acceleration value
+                        lastAcceleration = currentAcceleration;
+
+                        // calculate the current acceleration
+                        currentAcceleration = x * x + y * y + z * z;
+
+                        // calculate the change in acceleration
+                        acceleration = currentAcceleration *
+                                (currentAcceleration - lastAcceleration);
+
+                        // if the acceleration is above a certain threshold
+                        if (acceleration > ACCELERATION_THRESHOLD)
+                            //use supper if super is full
+                            Toast.makeText(getApplicationContext(), "Super Used", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                // required method of interface SensorEventListener
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+            };
+
+    // called by the system when the user either grants or denies the
+    // permission for saving an image
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        // switch chooses appropriate action based on which feature
+        // requested permission
+        switch (requestCode) {
+            case SAVE_RECORD_PERMISSION_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    // save the record if top 10
+                return;
+        }
     }
 }
