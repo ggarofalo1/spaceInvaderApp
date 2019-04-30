9ae9c1fb42fa3164d1627cd70eab04dd453a0b6d
ggarofalo1 <ggarofalo1@users.noreply.github.com>
2019-04-29 20:13:39 -0400

    added records

--- app/src/main/java/com/spaceisgreat/www/invaderapp/EndGameDialogFragment.java
+++ app/src/main/java/com/spaceisgreat/www/invaderapp/EndGameDialogFragment.java
@@ -3,20 +3,35 @@ package com.spaceisgreat.www.invaderapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class EndGameDialogFragment extends DialogFragment {
     private EditText usernameEditText;
     private TextView score;
+    private TextView Hscore;
     private GameSurface gamesurface;
+    private String[] highScore;
     private String username;
     private double timer;
+    public final String FILENAME ="gameRecord.txt";
+    private String record="";
 
     @Override
     public Dialog onCreateDialog(Bundle bundle) {
@@ -28,9 +43,14 @@ public class EndGameDialogFragment extends DialogFragment {
         timer = getArguments().getDouble("timer");
 
         score = gameoverDialogView.findViewById(R.id.displayscore);
+        Hscore = gameoverDialogView.findViewById(R.id.displayhighscore);
         usernameEditText = (EditText) gameoverDialogView.findViewById(R.id.usernameEditText);
 
+        username = usernameEditText.getText().toString();
+        highScore = getHighRecord(getContext(), FILENAME);
+
         score.setText(getResources().getString(R.string.score, timer));
+        Hscore.setText(getResources().getString(R.string.hscore)+ highScore[2]);
 
         builder.setPositiveButton(R.string.new_game,
                 new DialogInterface.OnClickListener() {
@@ -38,11 +58,59 @@ public class EndGameDialogFragment extends DialogFragment {
 
                         //username = usernameEditText.getText().toString();
                         gamesurface = new GameSurface(getContext());
+                        try{
+                            double hs =Double.parseDouble(highScore[2]);
+                            if(timer >= hs){
+                                record = ("1.0//"+username+"//"+Double.toString(timer));
+                                writeToRecords(getContext(),  FILENAME, record);
+                            }
+                        }catch(Exception e){
+
+                        }
+
                     }
                 }
         );
 
         return builder.create();
+    }
+
+    public void writeToRecords(Context context, String sFileName, String sBody) {
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "records");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File file = new File(root, sFileName);
+            FileWriter writer = new FileWriter(file);
+            writer.write(sBody);
+            writer.flush();
+            writer.close();
+            Toast.makeText(context, "Saved Record", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    public String[] getHighRecord(Context context, String sFileName) {
+        String High="";
+        String[] highscore = new String[0];
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "records");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File file = new File(root, sFileName);
+            FileReader reader = new FileReader(file);
+            int i;
+            while ((i=reader.read()) != -1)
+                High = (High +(char) i);
+
+            highscore = High.split("//", 4);
+        } catch (IOException e) {
+            //e.printStackTrace();
+        }
+        return highscore;
     }
 
     @Override
--- app/src/main/java/com/spaceisgreat/www/invaderapp/GreetingScreen.java
+++ app/src/main/java/com/spaceisgreat/www/invaderapp/GreetingScreen.java
@@ -1,12 +1,16 @@
 package com.spaceisgreat.www.invaderapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GreetingScreen extends AppCompatActivity {
 
@@ -53,8 +57,34 @@ public class GreetingScreen extends AppCompatActivity {
         help = findViewById(R.id.help);
         help.setOnClickListener(clickHelp);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permission denied to use your External Storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
 
 
     public void clickedPlaygame(){
